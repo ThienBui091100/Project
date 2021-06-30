9d0f20a5ab66970529c6cb74128526d58cba203f
ThienBui091100 <86742758+ThienBui091100@users.noreply.github.com>
2021-07-01 02:45:25 +0700

    Thien Bui

---
+++ 1811061027/1811061027/1811061027/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using _1811061027.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1811061027.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            var booklist = new List<Book>
+            {
+                new Book (){Bookid = 1, BookName = "abc",Page = 1 },
+                new Book (){Bookid = 2, BookName = "bac",Page = 2 },
+                new Book (){Bookid = 3, BookName = "acb",Page = 3 },
+                new Book (){Bookid = 4, BookName = "cab",Page = 4 },
+                new Book (){Bookid = 5, BookName = "cba",Page = 5 },
+
+            };
+            return View(booklist);
+        }
+    }
+}
---
+++ 1811061027/1811061027/1811061027/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1811061027.Models
+{
+    public class Book
+    {
+        public int Bookid { get; set; }
+        public string BookName { get; set; }
+        public int Page { get; set; }
+    }
+}
